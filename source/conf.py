7caea5e6a66aad7fd3226a4581af5114249d7f61
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2026-04-06 17:35:09 -0400

    $1

--- source/conf.py
+++ source/conf.py
@@ -52,6 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
+
 html_static_path = ['_static']
 
 html_css_files = [
